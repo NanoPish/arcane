bd948688838fae0e10e72517d02801e8b520f8f3
NanoPish <paul.belloc@epitech.eu>
2020-01-22 18:07:11 +0100

    make sqlite enforce FK constraints

--- app/arcane_real_estate/app/__init__.py
+++ app/arcane_real_estate/app/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_restplus import Api
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+from sqlite3 import Connection as SQLite3Connection
+
 
 db = SQLAlchemy()
 
+# make sqlite enforce FK constraints
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
 
 def create_app(env=None):
     from app.config import config_by_name
